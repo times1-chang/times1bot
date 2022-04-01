cd3213c32b0d38b9d867f3cde52efc4b5e725386
times1-chang <justin20050903@gmail.com>
2022-04-01 16:01:12 +0800

    fixed some problems

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-worker: python3 times1.py
+worker: python3 commands.py
--- commands.py
+++ commands.py
@@ -3,7 +3,7 @@ import asyncio
 from discord.ext import commands
 import os
 import json
-
+import random
 setdata = ''
 with open('setting.json','r', encoding="utf8") as setfile :
     setdata = json.load(setfile)
@@ -21,9 +21,20 @@ async def on_ready():
 async def ping(ctx):
     print('pinging')
     await ctx.send(f'{round(bot.latency*1000, 2)}ms')
+@bot.command()
+async def neko(ctx):
+    random_nekopic = random.choice(setdata['nekopic'])
+    await ctx.send(random_nekopic)
 
+@bot.command()
+async def vocaloid(ctx):
+    random_vcldpic = random.choice(setdata['vcldpic'])
+    await ctx.send(random_vcldpic)
 
-
+@bot.command()
+async def usefulpic(ctx):
+    random_usflpic = random.choice(setdata['usflpic'])
+    await ctx.send(random_usflpic)
 #@client.event
 #async def on_reaction_add(reaction, user):
 #    if
