6abdca831db6dd15bf03221c57672f713b970051
times1-chang <justin20050903@gmail.com>
2022-04-14 21:33:51 +0800

    add dailypic

--- main.py
+++ main.py
@@ -21,19 +21,26 @@ bot = commands.Bot(command_prefix='>', intents = intents)
 @bot.event
 async def on_ready():
     print('目前登入身份：', bot.user)
-    count = 7;
+    
     erochannel = bot.get_channel(setdata['erochannel'])
     for filename in os.listdir('.'):
         if filename.endswith('.py') and filename != 'main.py' and filename != 'classes.py':
             bot.load_extension(filename[:-3])
     while True:
-        url = f'https://acg.lspimg.com/archives/{count}/'
-        html = requests.get(url)
-        soup = BeautifulSoup(html.text, "html.parser")
-        results = soup.find_all("img", class_="post-item-img", limit=5)
-        image_links = list(result["src"] for result in results)
-        for link in image_links:
-            await erochannel.send(link)
-        count += 1;
-        await asyncio.sleep(24*60*60)
+        now = datetime.now()
+        hour = now.strftime('%H')
+        minute = now.strftime('%M')
+        print(hour)
+        if hour=="09" and minute =="00" :
+            print("send!")
+            mainhtml = requests.get("https://acg.lspimg.com")
+            mainsoup = BeautifulSoup(mainhtml.text, "html.parser")
+            newlink = mainsoup.find("a", class_="item-link")
+            html = requests.get(newlink["href"])
+            soup = BeautifulSoup(html.text, "html.parser")
+            results = soup.find_all("img", class_="post-item-img", limit=5)
+            image_links = list(result["src"] for result in results)
+            for link in image_links:
+                await erochannel.send(link)
+        await asyncio.sleep(60)
 bot.run(os.getenv('TOKEN'))
