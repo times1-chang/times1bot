ee5fa87492ec9a7aac97b0545d4e67faeb8828d4
times1-chang <justin20050903@gmail.com>
2022-04-29 16:50:32 +0800

    add help

--- commands.py
+++ commands.py
@@ -5,10 +5,29 @@ import os
 import json
 import random
 from classes import Cog_Extension
+import datetime
 setdata = ''
 with open('setting.json','r', encoding="utf8") as setfile :
     setdata = json.load(setfile)
 class CmdsClass(Cog_Extension):
+    @commands.command()
+    async def help(self, ctx):
+        embed=discord.Embed(title="Help", description="about times1 bot", color=0x720ab8)
+        embed.set_author(name="乘一", icon_url="https://64.media.tumblr.com/2061fee2529724d19321133f8b0e767d/e870751fd1d65c5f-c7/s400x600/d5020280b116871dbf935ac2837991ff3bdcd72d.jpg")
+        embed.set_thumbnail(url="https://d3mww1g1pfq2pt.cloudfront.net/Image/ckjconmb94vs408761xsx2ywp/1625477488941.jpg")
+        embed.add_field(name=">ping", value="", inline=True)
+        embed.add_field(name="", value="check network latency", inline=True)
+        embed.add_field(name=">roll value1 value2", value="", inline=True)
+        embed.add_field(name="", value="return random number\nfrom value1~value2\n(default is 1~30)", inline=True)
+        embed.add_field(name=">meme", value="", inline=True)
+        embed.add_field(name="", value="return random meme", inline=True)
+        embed.add_field(name=">neko", value="", inline=True)
+        embed.add_field(name="", value="return random neko picture", inline=True)
+        embed.add_field(name=">vocaloid", value="", inline=True)
+        embed.add_field(name="", value="return random vocaloid picture", inline=True)
+        embed.add_field(name="randh", value="", inline=True)
+        embed.add_field(name="", value="return random adult comic", inline=True)
+        await ctx.send(embed=embed)
     @commands.command()
     async def ping(self, ctx):
         print('pinging')
