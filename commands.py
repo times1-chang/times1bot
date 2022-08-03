7da58e0b9e42f9c4f10daf46fb1a1f0ffefb99a4
times1-chang <justin20050903@gmail.com>
2022-08-03 14:28:32 +0800

    add hcomic

--- commands.py
+++ commands.py
@@ -103,6 +103,15 @@ class CmdsClass(Cog_Extension):
                         await ctx.send("No Result!")
                     else:
                         await ctx.send(result["src"])
+    @commands.command()
+    async def hcomic(self, ctx, num: str):
+        if len(num)==6 and num.isdigit():
+            await ctx.send(f"https://nhentai.net/g/{num}")
+        if num=="rand" or num=="r" or num=="random":
+            await ctx.send(f'https://nhentai.net/g/{random.randint(100000,400000)}')
+        else:
+            ctx.send("I don't know what are you doing.\nUse >help command to see more information")
+
     #@client.event
 #async def on_reaction_add(reaction, user)
 def setup(bot):
